f2cc3e2613a5a589eb5dee5416650ddce3da6148
babujogi <122731100+babujogi@users.noreply.github.com>
2023-11-17 11:58:13 +0100

    Add files via upload

---
+++ abstract class.php
@@ -0,0 +1,16 @@
+<?php 
+
+    abstract class nazwa_klasy_abstrakcyjnej{
+        //szblon klasy
+    }
+
+    class nazwa_klasy extends nazwa_klasy_abstrakcyjnej{
+        //szabon
+    }
+
+    //interfejs
+    interface nazwa_interfejsu{
+        //szblon interfejsu
+    }
+
+?>
